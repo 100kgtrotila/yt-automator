7f3ef35ea53aa83abb14d6df04d45a24b2718ad6
100kgtrotila <hirodevi192@gmail.com>
2026-02-20 15:23:35 +0200

    feat: add configuration settings for video processing and YouTube

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+DATA_DIR = ROOT_DIR / "data"
+
+VIDEO_WIDTH = 1920
+VIDEO_HEIGHT = 1080
+FFMPEG_PRESET = "ultrafast"
+
+# Налаштування YouTube
+YOUTUBE_CATEGORY_ID = "10"
